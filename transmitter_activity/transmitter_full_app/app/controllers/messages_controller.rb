71a809d8a6c1e14c89c7a632a972ada05877cfd0
Sean Bjornsson <sean.bjornsson@annkissam.com>
2016-04-14 18:10:55 -0400

    update starter app to have decreased functionality

--- transmitter_activity/transmitter_full_app/app/controllers/messages_controller.rb
+++ transmitter_activity/transmitter_full_app/app/controllers/messages_controller.rb
@@ -10,27 +10,11 @@ class MessagesController < ApplicationController
 
     ip_addrs = incoming_message[:ip_addrs]
     next_addr = ip_addrs.first
-    new_ip_addr_list = ip_addrs[1..-1] << next_addr
-
-    broker_ip_addr = 'some.ip.addr.here'
+    new_ip_addr_list = ip_addrs[1..-1]
 
     sleep 1
 
-    Thread.new{
-      begin
-        send_message(incoming_message[:id], new_ip_addr_list, outgoing_message, "http://#{next_addr}:3000/messages")
-      rescue StandardError => e
-        Thread.new{ send_message(incoming_message[:id], "http://#{broker_ip_addr}:3000/messages", "http://#{broker_ip_addr}:3001/broker_errors") }
-      end
-    }
-
-    Thread.new{
-      begin
-        send_message(incoming_message[:id], new_ip_addr_list, outgoing_message, "http://#{broker_ip_addr}:3001/broker_messages")
-      rescue StandardError => e
-        puts e.message
-      end
-    }
+    send_message(incoming_message[:id], new_ip_addr_list, outgoing_message, "http://#{next_addr}:3000/messages")
 
     head :no_content, status: :ok
   end
@@ -55,49 +39,3 @@ class MessagesController < ApplicationController
     end
   end
 end
-
-
-
-
-
-# for testing purposes
-def test_post(ip, name, redirect_ip)
-  require 'faraday'
-
-  conn = Faraday.new(:url => "http://192.168.2.#{ip}:3000/messages")
-
-  message = {
-    data: {
-      ip_addrs: ["192.168.2.#{redirect_ip}"],
-      id: 1,
-      content: "Howdy #{name}",
-    }
-  }
-
-  # post payload as JSON instead of "www-form-urlencoded" encoding:
-  Thread.new do
-    conn.post do |req|
-      req.url '/messages'
-      req.headers['Content-Type'] = 'application/json'
-      req.body = message.to_json
-    end
-  end
-  ''
-end
-
-@teams = [
-  ["The Team That Must Not Be Named", "114"],
-  ["AJAx",                            "121"],
-  ["Eric's Folly",                    "192"],
-  ["Octavian's Left",                 "211"]
-]
-
-def ping_teams(redirect_ip)
-  @teams.each do |name, ip|
-    test_post(ip, name, redirect_ip)
-  end
-end
-
-# curl -H "Content-Type: application/json" -X POST -d '{"id":-1, "content":"all your base are"}' http://192.168.2.196:3000/messages
-
-
